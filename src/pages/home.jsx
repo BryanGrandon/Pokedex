455d0fc133f74115829b41dbc5899f7e8fb46016
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-06-25 22:04:29 -0400

    feat(web):add home pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "../components/header";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <article className="bg-shadow">
+      <Header />
+      <main>
+        <section>
+          <h2>Filter</h2>
+          <form>
+            <input type="radio" name="status" value="fire" />
+            <input type="radio" name="status" value="water" />
+          </form>
+        </section>
+      </main>
+      <footer></footer>
+    </article>
+  );
 }
 
 export default Home;
