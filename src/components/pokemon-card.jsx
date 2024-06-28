cab505b5928bed5aecdee69567f66a30b2c4dc67
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-06-28 18:02:21 -0400

    feat(web):change pokemon-card .jsx, .scss and export function

--- src/components/pokemon-card.jsx
+++ src/components/pokemon-card.jsx
@@ -1,30 +1,5 @@
 import React from "react";
-
-const getColor = (type) => {
-  const typeColor = {
-    bug: "#26de81",
-    dragon: "#ffeaa7",
-    electric: "#fed330",
-    fairy: "#FF0069",
-    fighting: "#30336b",
-    fire: "#f0932b",
-    flying: "#81ecec",
-    grass: "#00b894",
-    ground: "#EFB549",
-    ghost: "#a55eea",
-    ice: "#74b9ff",
-    normal: "#95afc0",
-    poison: "#6c5ce7",
-    psychic: "#a29bfe",
-    rock: "#2d3436",
-    water: "#0190FF",
-  };
-  let bg;
-  Object.entries(typeColor).forEach((color) => {
-    if (color[0] === type) bg = color[1];
-  });
-  return bg;
-};
+import { getPokemonColorType } from "../functions/getPokemonColorType";
 
 function PokemonCard({ name, number, types, img }) {
   let pokemonNumber = number.toString().padStart(4, "0");
@@ -34,15 +9,13 @@ function PokemonCard({ name, number, types, img }) {
     const info = {
       id: Math.floor(Math.random() * 2000),
       name: e.type.name,
-      color: getColor(e.type.name),
+      color: getPokemonColorType(e.type.name),
     };
     pokemonTypes.push(info);
   });
 
-  const bgColor = pokemonTypes[0].color + "9f";
-
   return (
-    <article className="pokemon" style={{ background: bgColor }}>
+    <article className="pokemon">
       <section>
         <p className="pokemon__number">N°{pokemonNumber}</p>
       </section>
