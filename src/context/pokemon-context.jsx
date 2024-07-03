d994862de105770b834bc7ab7306831009d3c32b
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-07-03 18:49:01 -0400

    refactor(web):add getAllPokemon

--- src/context/pokemon-context.jsx
+++ src/context/pokemon-context.jsx
@@ -11,32 +11,50 @@ const usePokemonContext = () => {
 
 function PokemonContextProvider({ children }) {
   const [offset, setOffset] = useState(0);
-  const [PokemonList, setPokemonList] = useState([]);
   const [saved, setSaved] = useState([]);
+  const [pokemonAll, setPokemonAll] = useState();
 
-  const getAllPokemon = async (limit = 20) => {
+  const getAllPokemon = async () => {
+    const urlAllPokemon = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
+    const result = await fetch(urlAllPokemon);
+    const dataAll = await result.json();
+    const dataPokemon = dataAll.results;
+    const pokemonNames = dataPokemon.map((e) => e.name);
+    const data = [];
+
+    dataPokemon.map((e) => {
+      const info = {
+        id: pokemonNames.indexOf(e.name) + 1,
+        name: e.name,
+        url: e.url,
+      };
+      data.push(info);
+    });
+    setPokemonAll(data);
+  };
+
+  const getTwentyPokemon = async (limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    const res = await fetch(url);
-    const data = await res.json();
+    const result = await fetch(url);
+    const data = await result.json();
     const promise = data.results.map(async (pokemon) => {
-      const res = await fetch(pokemon.url);
-      const data = await res.json();
+      const result = await fetch(pokemon.url);
+      const data = await result.json();
       return data;
     });
     const results = await Promise.all(promise);
 
-    setPokemonList([...PokemonList, ...results]);
-    setSaved([...PokemonList, ...results]);
-
+    setSaved([...saved, ...results]);
     setOffset(offset + 20);
   };
 
   const handlerClick = () => {
-    getAllPokemon();
+    getTwentyPokemon();
   };
 
   useEffect(() => {
     getAllPokemon();
+    getTwentyPokemon();
   }, []);
 
   return (
