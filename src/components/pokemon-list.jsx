81de795bf44cbe3b4299541fdfbd4f56a9956d85
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-07-05 20:37:44 -0400

    refactor(web):change pokemon-list.jsx for add new element

--- src/components/pokemon-list.jsx
+++ src/components/pokemon-list.jsx
@@ -6,31 +6,47 @@ function PokemonList() {
   let { saved } = usePokemonContext();
   let { handlerClick } = usePokemonContext();
 
-  // img = e.sprites.other.dream_world.front_default
   return (
     <article className="pokemon-article">
       <section className="pokemon-list">
-        {saved?.map((e) => (
-          <PokemonCard
-            key={e.id}
-            name={e.name}
-            number={e.id}
-            types={e.types}
-            img={
-              e.sprites.other.dream_world.front_default
-                ? e.sprites.other.dream_world.front_default
-                : e.sprites.other.home.front_default
-                ? e.sprites.other.home.front_default
-                : e.sprites.other["official-artwork"].front_default
-            }
-          />
-        ))}
+        {saved.length != 0
+          ? saved?.map((e) => (
+              <PokemonCard
+                key={e.id}
+                name={e.name}
+                number={e.id}
+                types={e.types}
+                img={
+                  e.sprites.other.dream_world.front_default
+                    ? e.sprites.other.dream_world.front_default
+                    : e.sprites.other.home.front_default
+                    ? e.sprites.other.home.front_default
+                    : e.sprites.other["official-artwork"].front_default
+                }
+              />
+            ))
+          : null}
       </section>
-      <ButtonMain
-        className="button-list"
-        children="Load more pokemon"
-        onClick={handlerClick}
-      />
+      {saved.length != 0 ? (
+        <ButtonMain
+          className="button-list"
+          children="Load more pokemon"
+          onClick={handlerClick}
+        />
+      ) : (
+        <section className="pokemon-alert">
+          <img
+            className="pokemon-alert__svg"
+            src="/src/assets/no-results-found.svg"
+            alt="no-results-found.png"
+          />
+
+          <h2 className="pokemon-alert__title">No results found</h2>
+          <p className="pokemon-alert__text">
+            We couldn't find what you're looking for.
+          </p>
+        </section>
+      )}
     </article>
   );
 }
