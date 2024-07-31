9f60acbc4ff8c702678083a6b9cdca13a010dd46
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-07-31 19:22:13 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pokemon/:id" element={<Pokemon />} />
         </Routes>
-      </BrowserRouter> */}
-      <h1>Hello</h1>
+      </BrowserRouter>
     </>
   );
 }
