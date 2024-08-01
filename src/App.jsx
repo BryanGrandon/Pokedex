f1f99d48c85fa33d10bca3c8361f9f085af11dbe
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-08-01 10:05:01 -0400

    remove component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import Pokemon from "./pages/pokemon";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon/:id" element={<Pokemon />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Hello</h1>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `https://BryanGrandon.github.io/pokedex`,
+  base: "https://BryanGrandon.github.io/Pokedex",
 });
