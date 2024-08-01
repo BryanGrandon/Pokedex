0cd1c4f9649b29ea05392a64ce6613fdd5116b40
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-08-01 10:29:18 -0400

    remove all

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import Pokemon from "./pages/pokemon";
-
 function App() {
   return (
     <>
       <h1>Hello</h1>
-      <Home />
     </>
   );
 }
