ad6c7f41c4b3f12b7c2b800ad13252b6cc8c8efd
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-08-01 10:38:35 -0400

    context and services

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { usePokemonContext } from "./context/pokemon-context";
+
 function App() {
+  let { saved } = usePokemonContext();
+
+  console.log(saved);
+
   return (
     <>
       <h1>Hello</h1>
