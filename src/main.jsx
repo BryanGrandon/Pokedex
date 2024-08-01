82025f2ebee4567c48cae3e03f39d50aa65d380a
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-08-01 10:10:12 -0400

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.scss";
+import { PokemonContextProvider } from "./context/pokemon-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PokemonContextProvider>
+      <App />
+    </PokemonContextProvider>
   </React.StrictMode>
 );
